6b29ad5ea3602799bcaf7455745efb106ff3365e
MaXo2 <yann.michel.maxo2@gmail.com>
2018-10-02 20:48:19 +0200

    back to the basic, ready to start

--- src/game/game.controller.js
+++ src/game/game.controller.js
@@ -13,21 +13,21 @@ function getNextPlayer(player, players) {
 function launchGame(id, nbCat, nbMaxPlayers, countDownBeforeGame) {
   let aGame = new Game(id, nbCat, nbMaxPlayers);
   aGame.init().then(() => {
-    aGame.newPlayer(); //simulation d'une connection d'un joueur
-    aGame.newPlayer(); //simulation d'une connection d'un joueur
-    aGame.newPlayer(); //simulation d'une connection d'un joueur
-    let player = pickAPlayer(aGame.players);
-    console.log('Le joueur qui joue : ' + player.name);
-    let result = launchDice();
-    console.log('Le résultat du dé : ' + result);
-    console.log(
-      aGame.board.getNeighbors(aGame.board.boxes[player.position], result)
-    );
-
-    player.position = 5; //CHOIX UTILISATEUR
-    console.log(player.name + ' se déplace sur la case n°' + player.position);
-
-    console.log(aGame.board.boxes[player.position]);
+    // aGame.newPlayer(); //simulation d'une connection d'un joueur
+    // aGame.newPlayer(); //simulation d'une connection d'un joueur
+    // aGame.newPlayer(); //simulation d'une connection d'un joueur
+    // let player = pickAPlayer(aGame.players);
+    // console.log('Le joueur qui joue : ' + player.name);
+    // let result = launchDice();
+    // console.log('Le résultat du dé : ' + result);
+    // console.log(
+    //   aGame.board.getNeighbors(aGame.board.boxes[player.position], result)
+    // );
+    //
+    // player.position = 5; //CHOIX UTILISATEUR
+    // console.log(player.name + ' se déplace sur la case n°' + player.position);
+    //
+    // console.log(aGame.board.boxes[player.position]);
   });
 }
 

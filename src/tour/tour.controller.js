0a474d98eb4e87d7cf1e30260941f90be5d2ba50
MaXo2 <yann.michel.maxo2@gmail.com>
2018-11-01 15:15:23 +0100

    function pickAQuestion, gameRound and  switching getQuestions to game.controller

--- src/tour/tour.controller.js
+++ src/tour/tour.controller.js
@@ -1,40 +1,30 @@
-const Question = require('./../question/question.model.js');
-
-function getQuestions() {
-  return new Promise((resolve, reject) => {
-    Question.findAll({}).then((questions) => {
-      let theQuestions = [];
-      questions.map((question) => {
-        const answers = question.responses;
-        const answerTrue = question.response;
-        answers.push(answerTrue);
-        answers.sort(function() {
-          return 0.5 - Math.random(); //un sort qui prend en paramètre entre -0.5 et 0.5
-        }); //donc random à chaque élèment
-        const theQuestion = {
-          id: question.id,
-          themeName: question.theme,
-          question: question.question,
-          correctAnswer: question.response,
-          answers: answers,
-        };
-        theQuestions.push({ content: theQuestion, isAlreadyUsed: false });
-      });
-
-      resolve(theQuestions);
-    });
-  });
-}
+const launchDice = require('../dice/dice.controller.js');
+const readlineSync = require('readline-sync');
+const {
+  NormalBox,
+  CheeseBox,
+  ReplayBox,
+  CenterBox,
+} = require('./../board/box.class.js');
 
 function pickAQuestion(questions, theme) {
   let themedQuestions = [];
+  let pickedIndex;
+  let theQuestion;
   questions.map((question) => {
-    if (question.themeName === theme) {
-      themedQuestions.push(question);
+    if (question.content.themeName === theme) {
+      themedQuestions.push(question.content);
     }
-    let pickedIndex = Math.floor(Math.random() * themedQuestions.length);
   });
-  return true;
+  pickedIndex = Math.floor(Math.random() * themedQuestions.length);
+  theQuestion = themedQuestions[pickedIndex];
+  questions.map((question) => {
+    if (question.content.id === theQuestion.id) {
+      question.isAlreadyUsed = true; //ça c'est complètement fou, ça modifie carrément le tableau originel passé en paramètre -_-
+    }
+  });
+  // TODO: ne pas choisir la question si elle a déja été utilisée
+  return theQuestion;
 }
 
 function displayQuestion(id) {
@@ -66,11 +56,101 @@ function resolveQuestion(id, reponseUser) {
   });
 }
 
+function gameRound(player, questions, board) {
+  console.info(player.name + ' joue !');
+  let result = launchDice();
+  console.info(player.name + ' lance le dé et fait ' + result);
+  console.info(
+    'Les cases à ' + result + ' déplacements de ' + player.name + ' sont : '
+  );
+
+  let voisins = board.getNeighbors(board.boxes[player.position], result);
+  let indexVoisins = [];
+  let casesVoisines = [];
+  voisins.forEach((voisin) => {
+    indexVoisins.push(voisin._id);
+    if (voisin instanceof CheeseBox) {
+      casesVoisines.push(
+        'Case #' +
+          voisin._id +
+          ' [CASE CAMEMBERT] : Theme "' +
+          voisin._theme.name +
+          '"'
+      );
+    } else if (voisin instanceof ReplayBox) {
+      casesVoisines.push('Case #' + voisin._id + ' [CASE REPLAY]');
+    } else if (voisin instanceof CenterBox) {
+      casesVoisines.push('Case #' + voisin._id + ' [CASE CENTRALE]');
+    } else {
+      casesVoisines.push(
+        'Case #' + voisin._id + ' : Theme "' + voisin._theme.name + '"'
+      );
+    }
+  });
+
+  player.position =
+    indexVoisins[
+      readlineSync.keyInSelect(
+        casesVoisines,
+        'Sur quelle case ' + player.name + ' souhaite-t-il se deplacer ?',
+        {
+          cancel: false,
+        }
+      )
+    ]; //CHOIX UTILISATEUR
+  if (
+    board.boxes[player.position] instanceof NormalBox ||
+    board.boxes[player.position] instanceof CheeseBox ||
+    board.boxes[player.position] instanceof CenterBox
+  ) {
+    let theme;
+    if (board.boxes[player.position] instanceof CenterBox) {
+      theme =
+        board.themes[Math.floor(Math.random() * board.themes.length)].name;
+      console.info(
+        player.name +
+          ' se déplace sur la case Centrale dont la catégorie aléatoire est "' +
+          theme +
+          '"'
+      );
+    } else {
+      theme = board.boxes[player.position].theme.name;
+      console.info(
+        player.name +
+          ' se déplace sur la case n°' +
+          player.position +
+          ' dont la catégories est "' +
+          theme +
+          '"'
+      );
+    }
+    let question = pickAQuestion(questions, theme);
+    console.log('[' + question.themeName + '] ' + question.question);
+    console.log("[Bonne réponse : '" + question.correctAnswer + "']");
+    let inputUser = readlineSync.keyInSelect(
+      question.answers,
+      'Quelle est la bonne réponse ?',
+      {
+        cancel: false,
+      }
+    );
+    if (question.answers[inputUser] === question.correctAnswer) {
+      console.log('Bien ouèj, gros !!');
+      return true;
+    } else {
+      console.log('looser !');
+      return false;
+    }
+  } else {
+    return true;
+  }
+}
+
 module.exports = {
-  getQuestions,
   pickAQuestion,
   displayQuestion,
   resolveQuestion,
+  gameRound,
 };
 
 //Getquestions(theme)
